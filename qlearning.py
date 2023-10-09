2654f8473970603cf5b5ead9776b9bb8fd307ac7
Martin Skjelvareid <martin.hansen.skjelvareid@gmail.com>
2023-10-09 16:47:16 +0200

    Adding assignment text and empty code frame

---
+++ qlearning.py
@@ -0,0 +1,30 @@
+# You can import matplotlib or numpy, if needed
+# You can also import any module included in Python 3.10, for example "random"
+# See https://docs.python.org/3.10/py-modindex.html for included modules
+
+class Robot:
+
+    def __init__():
+        # Define R- and Q-matrices here 
+        pass
+
+    def get_next_state_mc():
+        pass
+
+    def get_next_state_eg(): # Only implement this if using "epsilon-greedy" policy
+        pass
+
+    def monte_carlo_exploration():
+        pass
+
+    def q_learning():
+        pass
+
+    def greedy_path():
+        pass
+
+if __name__ == "__main__":
+    # Run Q-learning experiments and show results
+    pass
+
+# Feel free to add additional classes / methods / functions to answer the assignment.
